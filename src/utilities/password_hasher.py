9e26e33e46a45abdde73f81274768001be96c3c7
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-12-06 14:54:14 +0400

    replace sha256 with bcrypt;

--- src/utilities/password_hasher.py
+++ src/utilities/password_hasher.py
@@ -1,7 +1,9 @@
 import os
 import string
-import hashlib
+# import hashlib
+import bcrypt
 import secrets
+import random
 
 
 class Password:
@@ -10,21 +12,31 @@ class Password:
     __sprinkles = string.ascii_letters + string.digits
 
     @staticmethod
-    def __hash(payload: str) -> str:
-        return hashlib.sha256(payload.encode()).hexdigest()
+    def __hash(payload: bytes) -> bytes:
+        # return hashlib.sha256(payload.encode()).hexdigest()
+        salt = bcrypt.gensalt(8)
+        return bcrypt.hashpw(payload, salt)
+
 
     @classmethod
     def new(cls, plain_text_password: str) -> str:
         sprinkle = secrets.choice(cls.__sprinkles)
         payload = f"{cls.__salt}{plain_text_password}{cls.__pepper}{sprinkle}"
-        return cls.__hash(payload)
+        return cls.__hash(payload.encode("ascii")).decode("ascii")
 
     @classmethod
     def compare(cls, hashed_password: str, plain_text_password: str) -> bool:
         for sprinkle in cls.__sprinkles:
             payload = f"{cls.__salt}{plain_text_password}{cls.__pepper}{sprinkle}"
 
-            if cls.__hash(payload) == hashed_password:
+            if bcrypt.checkpw(payload.encode("ascii"), hashed_password.encode("ascii")):
                 return True
 
         return False
+
+
+# unittest
+def test_password_compare():
+    password = ''.join(random.choices(string.printable[:-5], k=random.randint(8, 64)))
+    password_hash = Password.new(password)
+    assert Password.compare(password_hash, password), "passwords do not match"
