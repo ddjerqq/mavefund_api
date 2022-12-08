cf05395efe1627971cf50509edba00b941f2d5a8
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-12-08 14:52:27 +0400

    fix bug in html;

--- static/js/script.js
+++ static/js/script.js
@@ -1,50 +1,94 @@
-async function login(e) {
-    e.preventDefault();
+async function login(_) {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const error_el = document.getElementById("error");
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    const username_regex = /^[a-zA-Z0-9._]{3,32}$/;
+    // password regex with at least 8 characters, 1 uppercase, 1 lowercase, 1 number
+    const password_regex = /^.{8,64}$/;
+
+    if (!username_regex.test(username)) {
+        error_el.textContent = "Invalid username";
+        return;
+    }
+
+    if (!password_regex.test(password)) {
+        error_el.textContent = "Invalid password, must be at least 8 characters";
+        return;
+    }
 
     try {
         const response = await fetch("/auth/login", {
             method: "POST",
             headers: new Headers({
-                "Content-Type": "application/json"
+                "content-type": "application/json"
             }),
-            redirect: "follow",
             body: JSON.stringify({
                 "username": username,
                 "password": password
             }),
         });
 
-        if (response.status === 200) {
-            const token = await response.json();
-            localStorage.setItem("token", token);
-            localStorage.setItem("username", username);
+        switch (response.status) {
+            case 200:
+                const token = await response.json();
+                localStorage.setItem("token", token);
+                localStorage.setItem("username", username);
 
-            document.cookie = `token=${token}`;
+                document.cookie = `token=${token}`;
 
-            window.location.replace("/");
-        }
-        else {
-            alert(`Error`);
+                window.location.replace("/");
+                break;
+
+            case 404:
+                error_el.textContent = "Username is not registered, please register";
+                break;
+
+            case 400:
+                error_el.textContent = "Password is incorrect";
+                break;
+
+            default:
+                let err = await response.json();
+                error_el.textContent = `Something went wrong. ${err.detail}`;
+                break;
         }
     }
     catch (error) {
         console.log(error);
+        error_el.textContent = "Something went wrong. Please try again later!";
     }
 }
 
+async function register(_) {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const repeat_password = document.getElementById("repeat_password").value;
+    const error_el = document.getElementById("error");
 
-async function register(e) {
-    e.preventDefault();
+    const username_regex = /^[a-zA-Z0-9._]{3,32}$/;
+    const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,255}$/;
+    // password regex with at least 8 characters, 1 uppercase, 1 lowercase, 1 number
+    const password_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,64}$/;
 
-    let username = document.getElementById("username").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+    if (!username_regex.test(username)) {
+        error_el.textContent = "Invalid username";
+        return;
+    }
 
-    if (username === "" || email === "" || password === "") {
-        alert("Please fill in all fields");
+    if (!email_regex.test(email)) {
+        error_el.textContent = "Invalid email";
+        return;
+    }
+
+    if (!password_regex.test(password)) {
+        error_el.textContent = "Invalid password, must be at least 8 characters, 1 uppercase, 1 lowercase, 1 number";
+        return;
+    }
+
+    if (password !== repeat_password) {
+        error_el.textContent = "Passwords do not match";
         return;
     }
 
@@ -52,9 +96,8 @@ async function register(e) {
         const response = await fetch("/auth/register", {
             method: "POST",
             headers: {
-               "Content-Type": "application/json"
+               "content-type": "application/json"
             },
-            redirect: "follow",
             body: JSON.stringify({
                 "username": username,
                 "email": email,
@@ -62,21 +105,54 @@ async function register(e) {
             }),
         });
 
-        if (response.status === 200) {
-            const token = await response.json();
-            localStorage.setItem("token", token);
-            localStorage.setItem("username", username);
+        const detail = await response.json();
 
-            document.cookie = `token=${token}`;
+        switch (response.status) {
+            case 200:
+                const token = await response.json();
+                localStorage.setItem("token", token);
+                localStorage.setItem("username", username);
 
-            // redirect to index.
-            window.location.replace("/");
-        }
-        else {
-            alert(`Error`);
+                document.cookie = `token=${token}`;
+
+                // redirect to index.
+                window.location.replace("/");
+                break;
+
+            case 409:
+                if (detail.includes("username")) {
+                    error_el.textContent = "Username is already registered";
+                }
+                else if (detail.includes("email")) {
+                    error_el.textContent = "Email is already registered";
+                }
+                else {
+                    error_el.textContent = `Something went wrong. ${detail}`;
+                }
+                break;
+
+            default:
+                error_el.textContent = `Something went wrong. ${detail}`;
+                break;
         }
 
     } catch (error) {
         console.log(error);
     }
+}
+
+
+// todo make searchbar work
+async function submit(e) {
+    console.log(e);
+    e.preventDefault();
+
+    let ticker = document.getElementById("ticker").value;
+
+    if (ticker === "") {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    alert(`you are searching for ${ticker}`);
 }
