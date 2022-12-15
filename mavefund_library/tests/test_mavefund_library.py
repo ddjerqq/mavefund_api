5230f8d73c0776a945bae115505b8410b97f2f48
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-12-15 19:08:20 +0400

    update test_mavefund_library.py;

--- mavefund_library/tests/test_mavefund_library.py
+++ mavefund_library/tests/test_mavefund_library.py
@@ -2,6 +2,6 @@ from mavefund_library.src import Client
 
 client = Client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDUyMjY3NDY5NDAyNzE2NzAiLCJleHAiOjE2NzA5NTQ2MjR9.sLL62bbTHjcw0Fa19RtQ7Bo5w1k6BWXD3EzHhtjqnNw")
 
-
+# TODO this is getting imported while we are running other tests.
 data = client.get_records_as_df("AAPL")
 print(data)
