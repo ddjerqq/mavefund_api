2ea31af36ea37a0cd52482a25543c7033f956c5a
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2022-12-15 17:02:16 +0400

    add test_mavefund_library.py;

---
+++ mavefund_library/tests/test_mavefund_library.py
@@ -0,0 +1,7 @@
+from mavefund_library.src import Client
+
+client = Client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDUyMjY3NDY5NDAyNzE2NzAiLCJleHAiOjE2NzA5NTQ2MjR9.sLL62bbTHjcw0Fa19RtQ7Bo5w1k6BWXD3EzHhtjqnNw")
+
+
+data = client.get_records_as_df("AAPL")
+print(data)
