1d6841ce23aa097b99c15487b6aaca5d1d970695
Giorgi <57017344+ddjerqq@users.noreply.github.com>
2023-01-06 17:16:23 +0400

    update index_router.py

--- src/utilities/__init__.py
+++ src/utilities/__init__.py
@@ -2,3 +2,4 @@ from .password_hasher import Password
 from .snowflake import Snowflake
 from .render_template import render_template
 from .extract_claims_from_jwt import extract_claims_from_jwt
+from .get_stock_price import get_stock_price
