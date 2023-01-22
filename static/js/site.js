82c1ce026cc99fdf545d7c2adf40641f817d67dc
Joseph Safwat Khella <josephskhella@gmail.com>
2023-01-22 19:59:29 +0200

    fixes

--- static/js/site.js
+++ static/js/site.js
@@ -50,24 +50,12 @@ $(document).ready(function() {
               <div class="DocSearch-Hit-Container">
                 <div>
                   <div class="DocSearch-Hit-icon">
-                    <img src="https://logo.clearbit.com/${res[key]}.com" height="20px" width="20px"/>
                   </div>
                   <div class="DocSearch-Hit-content-wrapper"><span class="DocSearch-Hit-title"><strong>${key}</strong> ${res[key]}</span>
                   </div>
                 </div>
                 <div>
-                  <div class="DocSearch-Hit-action"><button class="DocSearch-Hit-action-button"
-                      title="Save this search" type="submit"><svg width="20" height="20" viewBox="0 0 20 20">
-                        <path d="M10 14.2L5 17l1-5.6-4-4 5.5-.7 2.5-5 2.5 5 5.6.8-4 4 .9 5.5z"
-                          stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linejoin="round"></path>
-                      </svg></button></div>
-                  <div class="DocSearch-Hit-action"><button class="DocSearch-Hit-action-button"
-                      title="Remove this search from history" type="submit"><svg width="20" height="20"
-                        viewBox="0 0 20 20">
-                        <path d="M10 10l5.09-5.09L10 10l5.09 5.09L10 10zm0 0L4.91 4.91 10 10l-5.09 5.09L10 10z"
-                          stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round"
-                          stroke-linejoin="round"></path>
-                      </svg></button></div>
+                 
                 </div>
               </div>
             </a>
